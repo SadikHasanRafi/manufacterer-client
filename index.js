1869602270a0b945b7f0272802b41bcc8a69dafc
SadikHasanRafi <sadikkhasan@gmail.com>
2022-05-28 14:33:13 +0600

    insertion operation done and review add done

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId
 const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 8000;
@@ -6,6 +8,47 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+//sadik192
+//8L1GXEgQxv6QEfAh
+
+
+const uri = "mongodb+srv://sadik192:8L1GXEgQxv6QEfAh@cluster0.hy9z3.mongodb.net/?retryWrites=true&w=majority";
+const server = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+const orderListCollection = server.db("laptopAccessories").collection("orderList");
+const productsCollection = server.db("laptopAccessories").collection("products");
+const reviewsCollection = server.db("laptopAccessories").collection("reviews");
+const devicesCollection = server.db("laptopAccessories").collection("users");
+  
+
+//add data to db
+async function addingData(data,dataCollection){
+    try {
+        await server.connect();
+        const result = await dataCollection.insertOne(data)
+        console.log(`User inserted with id: ${result.insertedId}`)
+    } finally {
+        await server.close();        
+    }
+}
+
+
+
+//adding review api
+
+app.post('/addreview',(req,res) => {
+    const newReview = req.body;
+    addingData(newReview,reviewsCollection).catch(console.dir)
+    res.send({success:true})
+})
+
+
+
+
+
+
 
 
 
